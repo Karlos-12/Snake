4c5257300c4b33558a2fcebad7b49e6aeb20f2e2
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-05-31 09:40:40 +0200

    sdfsd

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -23,13 +23,15 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool lose = false;
         int x = 8;
         int y = 4;
 
+        int xkostka = 0;
+        int ykostka = 0;
+
         int round = 0;
         int way = 0;
-        int leng = 10;
+        int leng = 5;
 
         List<int> xs = new List<int>();
         List<int> ys = new List<int>();
@@ -37,12 +39,39 @@ namespace Snake
         public MainWindow()
         {
             InitializeComponent();
-            
-
         }
+        Rectangle kostka = new Rectangle
+        {
+            Height = 100,
+            Width = 100,
+            Fill = new SolidColorBrush(Color.FromRgb(128, 128, 0))
+        };
+
+        public void genratorjakdebil()
+        {
+            
+            Random random = new Random();
+            bool does = true;
+            do
+            {  
+                ykostka = random.Next(8);
+                xkostka = random.Next(8);
+                for(int i = xs.Count - 1; i > 0; i--)
+                {
+                    if(xs[i] != xkostka && ys[i] != ykostka)
+                    {
+                        does = false;
+                    }
+                }
+            }
+            while(does == true);
+            
+        }
+        int exercontrolníhovno = 0;
 
         public void Snake(object source, EventArgs e)
         {
+            exercontrolníhovno--;
             round++;
             main.Children.Clear();
             switch (way)
@@ -93,7 +122,40 @@ namespace Snake
             }
             listup();
             Draw();
+
+            for(int i = xs.Count - 1; i > 0; i--)
+            {
+                if (xs[i] == x && ys[i] == y)
+                {
+                    ende();
+                }
+            }
+            if(exercontrolníhovno < 0)
+            {
+                if (x == xkostka && y == ykostka)
+                {
+                    genratorjakdebil();
+                    leng++;
+                }
+            }
+
+           if(exercontrolníhovno == 0)
+            {
+                genratorjakdebil();
+            }
+
+            main.Children.Add(kostka);
+            Canvas.SetLeft(kostka, xkostka * 100);
+            Canvas.SetTop(kostka, ykostka * 100);
         }
+
+        public void ende()
+        {
+            MessageBox.Show("najs more si debil fakt chudák žížala");
+            myTimer.Stop();
+            main.Children.Clear();
+        }
+
         Rectangle head = new Rectangle
         {
             Height = 100,
@@ -102,16 +164,16 @@ namespace Snake
         };
 
         public void Draw()
-        {
-            
+        {         
             main.Children.Add(head);
             Canvas.SetLeft(head, x * 100);
             Canvas.SetTop(head, y * 100);
         }
-
+        DispatcherTimer myTimer = new DispatcherTimer();
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DispatcherTimer myTimer = new DispatcherTimer();
+            exercontrolníhovno = leng;
+
             myTimer.Tick += new EventHandler(Snake);
             myTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
 
@@ -125,8 +187,6 @@ namespace Snake
             }
 
             myTimer.Start();
-
-           
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -151,17 +211,35 @@ namespace Snake
 
         public void listup()
         {
+            int savyx = 0;
+            int svaey = 0;
             for(int i = leng; i > 0; i--)
             {
-                xs[i] = xs[i -1];
-                ys[i] = ys[i -1];
+                if(i > xs.Count-1 || i > ys.Count-1)
+                {
+                    savyx = xs[i];
+                    svaey = ys[i];
+                }
+                else
+                {
+                    xs[i] = xs[i - 1];
+                    ys[i] = ys[i - 1];
+                }
+
             }
             xs[0] = x;
             ys[0] = y;
 
             for(int i = leng; i > 0; i--)
             {
-                drawexces(xs[i], ys[i], main);
+                if (i > xs.Count - 1 || i > ys.Count - 1)
+                {
+                    drawexces(savyx, savyx, main);
+                }
+                else
+                {
+                    drawexces(xs[i], ys[i], main);
+                }
             }
         }
 
